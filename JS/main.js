87389096fd8715fbeb59cb5809e1864129d658bc
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-05-20 15:42:35 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,26 @@
-var fila = new fila();
-var pilha = new pilha();
+var cliente = new fila();
+var fornecedor = new pilha();
 
 function enqueue_fila(nome, sobrenome, email, cpf, telefone){
-  fila.enqueue(nome, sobrenome, email, cpf, telefone);
+  cliente.enqueue(nome, sobrenome, email, cpf, telefone);
 } 
 
 function dequeue_fila(){
-  return fila.dequeue();
+  return cliente.dequeue();
 }
 
 function print_fila(){
-  return fila.imprimeFila();
+  return cliente.imprimeFila();
 }
 
 function push_pilha(cnpj, razao_social, nome_fantasia, email, telefone){
-  pilha.push(cnpj, razao_social, nome_fantasia, email, telefone);
+  fornecedor.push(cnpj, razao_social, nome_fantasia, email, telefone);
 }
 
 function pop_pilha(){
-  return pilha.pop();
+  return fornecedor.pop();
 }
 
 function print_pilha(){
-  return pilha.imprimePilha();
+  return fornecedor.imprimePilha();
 }
