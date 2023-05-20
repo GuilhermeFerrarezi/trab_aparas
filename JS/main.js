b1aa00d5868649ff717bbffd74269d4fa06c4aac
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-05-20 19:30:23 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,17 @@
 var cliente = new fila();
 var fornecedor = new pilha();
 
-function enqueue_fila(nome, sobrenome, email, cpf, telefone){
-  cliente.enqueue(nome, sobrenome, email, cpf, telefone);
+var form1 = document.getElementById('formulario_cliente');
+var form2 = document.getElementById('formulario_forn');
+
+function enqueue_fila(){
+  let nome1 = document.getElementById('nome').value;
+  let sobrenome1 = document.getElementById('sobrenome').value;
+  let cpf1 = document.getElementById('cpf').value;
+  let email1 = document.getElementById('email').value;
+  let telefone1 = getElementById('telefone').value;
+  cliente.enqueue(nome1, sobrenome1, email1, cpf1, telefone1);
+  console.log()
 } 
 
 function dequeue_fila(){
@@ -13,8 +22,13 @@ function print_fila(){
   return cliente.imprimeFila();
 }
 
-function push_pilha(cnpj, razao_social, nome_fantasia, email, telefone){
-  fornecedor.push(cnpj, razao_social, nome_fantasia, email, telefone);
+function push_pilha(){
+  let nome_fantasia2 = document.getElementById('nomeFantasia').value;
+  let razao_social2 = document.getElementById('razaoSocial').value;
+  let cnpj2 = document.getElementById('cnpj').value;
+  let email2 = document.getElementById('emailforn').value;
+  let telefone2 = document.getElementById('telefoneforn').value;
+  fornecedor.push(cnpj2, razao_social2, nome_fantasia2, email2, telefone2);
 }
 
 function pop_pilha(){
