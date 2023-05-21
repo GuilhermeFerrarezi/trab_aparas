41783f6c6f6cab21a6851c0ea407961e60afc7b3
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-05-20 21:42:19 -0300

    Update pilha.js

--- JS/pilha.js
+++ JS/pilha.js
@@ -6,7 +6,7 @@ class pilha {
   }
 
   push(cnpj, razao_social, nome_fantasia, email, telefone) {
-    let no_trab = new node_fornecedor(cnpj, razao_social, nome_fantasia, email, telefone);
+    let no_trab = new Node_fornecedor(cnpj, razao_social, nome_fantasia, email, telefone);
     if (this.size == 0) {
       this.head = no_trab;
       this.tail = no_trab;
@@ -21,8 +21,7 @@ class pilha {
   pop() {
     let retorno;
     if (this.size == 0) {
-      console.log("Lista vazia");
-      return null;
+      return "Lista vazia";
     } else if (this.size == 1) {
       retorno = this.tail;
       this.head = null;
@@ -40,13 +39,12 @@ class pilha {
     let no_trab = this.head;
     let retorno = "";
     if (this.size == 0) {
-      console.log("Lista vazia");
+      return "Lista vazia";
     } else {
       for(let i = 0; i < this.size; i++) {
         retorno += ("CNPJ: " + no_trab.cnpj + "\nRazão Social: " + no_trab.razao_social + "\nNome Fantasia: " + no_trab.nome_fantasia + "\nEmail: " + no_trab.email + "\nTelefone: " + no_trab.telefone + "\n\n");
         no_trab = no_trab.next;
       }
-      console.log(retorno);
       return retorno;
     }
   }
